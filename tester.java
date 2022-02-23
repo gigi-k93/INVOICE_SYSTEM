dbb4c1af93e12cb72c4c7fad031796e88d245672
Gloria Kyei <67934038+gigi-k93@users.noreply.github.com>
2022-02-24 00:37:02 +0200

    Add files via upload

---
+++ tester.java
@@ -0,0 +1,14 @@
+ @author gloria
+ */
+public class InvoiceSystemTester {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     invoice_system i=new invoice_system();
+     i.drawAndShowGUI();
+     
+    }
+    
+}
